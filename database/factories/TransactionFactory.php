47bb2a063a81bb501c8aaf884ba1f11cb1fc64f5
acatzk <joshuaimalay@gmail.com>
2022-07-12 13:53:52 +0800

    feat: add transaction factory fake data

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Seller;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+
+        $seller = Seller::has('products')->get()->random();
+        $buyer = User::all()->except($seller->id)->random();
+
+        return [
+            'quantity' => fake()->numberBetween(1, 3),
+            'buyer_id' => $buyer->id,
+            'product_id' => $seller->products->random()->id
+        ];
+    }
+}
