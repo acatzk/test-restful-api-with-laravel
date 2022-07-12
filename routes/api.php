cc90a743e476a92df8497fc705e7e42424942eea
acatzk <joshuaimalay@gmail.com>
2022-07-12 10:57:10 +0800

    feat: create resful-api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +18,50 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/*
+ *
+ * Buyers 
+ * 
+ */
+
+Route::resource('buyers', BuyerController::class, ['only' => ['index', 'show']]);
+
+
+/*
+ *
+ * Categories 
+ * 
+ */
+Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
+
+
+/*
+ *
+ * Products 
+ * 
+ */
+Route::resource('products', ProductController::class, ['only' => ['index', 'show']]);
+
+
+/*
+ *
+ * Sellers 
+ * 
+ */
+Route::resource('sellers', SellerController::class, ['only' => ['index', 'show']]);
+
+
+/*
+ *
+ * Transactions 
+ * 
+ */
+Route::resource('transactions', TransactionController::class, ['only' => ['index', 'show']]);
+
+/*
+ *
+ * Users 
+ * 
+ */
+Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
