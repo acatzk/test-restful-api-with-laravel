c0f73bada9bd799af8ca015aca94a40675147cc8
acatzk <joshuaimalay@gmail.com>
2022-07-16 12:28:01 +0800

    implement soft deleting for all the models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     const AVAILABLE_PRODUCT = 'available';
     const UNAVAILABLE_PRODUCT = 'unavailable';
