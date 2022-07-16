2e23cd83273a3c23f40e05ce8de6ea8bb7af1eeb
acatzk <joshuaimalay@gmail.com>
2022-07-16 12:10:53 +0800

    feat: implicit model binding routes and methods

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -26,9 +26,9 @@ class SellerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Seller $seller)
     {
-        $seller = Seller::has('transactions')->findOrFail($id);
+        // $seller = Seller::has('transactions')->findOrFail($id);
 
         return $this->showOne($seller);
     }
